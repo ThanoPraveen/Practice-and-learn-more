049e82911dfb143e0efb2600e9bf886de5231994
MPK PRAVEEN KUMAR <mpkthanos428@gmail.com>
2022-05-10 19:22:52 +0530

    change and add small step of animation

--- According/script.js
+++ According/script.js
@@ -5,10 +5,13 @@ for (point = 0; point < accord.length; point++) {
   accord[point].addEventListener("click", function () {
     this.classList.toggle("active");
     var panel = this.nextElementSibling;
-    if (panel.style.display === "block") {
-      panel.style.display = "none";
+    // if (panel.style.display === "block") { // normal method
+    if (panel.style.maxHeight) {
+      // panel.style.display = "none"; // normal method
+      panel.style.maxHeight = null;
     } else {
-      panel.style.display = "block";
+      // panel.style.display = "block"; // normal method
+      panel.style.maxHeight = panel.scrollHeight + "px";
     }
   });
 }
