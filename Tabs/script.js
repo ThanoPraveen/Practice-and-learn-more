07337bc4938bb2139df891be1956c82fe1723330
MPK PRAVEEN KUMAR <mpkthanos428@gmail.com>
2022-05-13 20:16:50 +0530

    Add a new learn Tab in single page

---
+++ Tabs/script.js
@@ -0,0 +1,21 @@
+function openCity(evt, cityName) {
+  // Declare all variables
+  var i, tabContent, tabLinks;
+
+  // Get all elements with class="tabContent" and hide them
+  document.getElementById("defaultOpen").click();
+  tabContent = document.getElementsByClassName("tabContent");
+  for (i = 0; i < tabContent.length; i++) {
+    tabContent[i].style.display = "none";
+  }
+
+  // Get all elements with class="tabLinks" and remove the class "active"
+  tabLinks = document.getElementsByClassName("tabLinks");
+  for (i = 0; i < tabLinks.length; i++) {
+    tabLinks[i].className = tabLinks[i].className.replace(" active", "");
+  }
+
+  // Show the current tab, and add an "active" class to the button that opened the tab
+  document.getElementById(cityName).style.display = "block";
+  evt.currentTarget.className += " active";
+}
